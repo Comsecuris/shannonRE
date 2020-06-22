14cb573e816ea0b06aff26ed49e9ebd62578181b
Brinly <brinly@brinly.me>
2020-06-22 13:20:51 +1000

    Update find function prologues script for IDA 7.5+python3
    IDA's API changed.
    
    I haven't pushed all the updates (and various improvements) for these scripts but this will do for now.

--- idapython/plugins/def_arm32_functions.py
+++ idapython/plugins/def_arm32_functions.py
@@ -8,17 +8,17 @@ def def_functions(s_start):
     num_added_functions = 0
 
     s_addr = s_start
-    s_end = idc.GetSegmentAttr(s_start, SEGATTR_END) #idc.SegEnd(segm)
-    print "0x%08x 0x%08x" % (s_start, s_end) 
+    s_end = idc.get_segm_attr(s_start, SEGATTR_END) #idc.SegEnd(segm)
+    print("0x%08x 0x%08x" % (s_start, s_end)) 
     
     while (s_addr < s_end):
 
-        print "Testing address 0x%08x" % s_addr
+        print("Testing address 0x%08x" % s_addr)
         
         #optimization assumes that function chunks are consecutive (no "function-in-function" monkey business)
         if (idaapi.get_func(s_addr)):
             
-            next_func = idc.NextFunction(s_addr)
+            next_func = idc.get_next_func(s_addr)
 
             ea = s_addr
             for c in idautils.Chunks(s_addr):
@@ -40,12 +40,12 @@ def def_functions(s_start):
                 continue
             '''
             #also add STMFD 
-            if ((idc.GetMnem(s_addr) == "STM") and ("SP!" in idc.GetOpnd(s_addr, 0)) and ("LR" in idc.GetOpnd(s_addr, 1))) or (((idc.GetMnem(s_addr) == "PUSH") or (idc.GetMnem(s_addr) == "PUSH.W") or (idc.GetMnem(s_addr) == "STR.W") ) and ("LR" in idc.GetOpnd(s_addr, 0))):
-                print "Found function at 0x%08x" % s_addr
-                idc.MakeFunction(s_addr)
+            if ((idc.print_insn_mnem(s_addr) == "STM") and ("SP!" in idc.print_operand(s_addr, 0)) and ("LR" in idc.print_operand(s_addr, 1))) or (((idc.print_insn_mnem(s_addr) == "PUSH") or (idc.print_insn_mnem(s_addr) == "PUSH.W") or (idc.print_insn_mnem(s_addr) == "STR.W") ) and ("LR" in idc.print_operand(s_addr, 0))):
+                print("Found function at 0x%08x" % s_addr)
+                ida_funcs.add_func(s_addr)
                 f = idaapi.get_func(s_addr)
                 if (type(f) == type(None)):
-                    print "Failed to create function! Undefined instructions?"
+                    print("Failed to create function! Undefined instructions?")
                     s_addr += 2
                 else:
                     num_added_functions += 1
@@ -61,7 +61,7 @@ def def_functions(s_start):
             else:
                 s_addr += 2
 
-    print "finished segment"
+    print("finished segment")
     return num_added_functions
  
 
@@ -69,7 +69,7 @@ num_total_added_functions = 0
 for s in idautils.Segments():
     s_start = s   
     if idaapi.segtype(s_start) == idaapi.SEG_CODE:
-        print "starting segment at 0x%08x" % s_start
+        print("starting segment at 0x%08x" % s_start)
         num_total_added_functions += def_functions(s)
 
-print "Added %d functions in total" % num_total_added_functions
+print("Added %d functions in total" % num_total_added_functions)
